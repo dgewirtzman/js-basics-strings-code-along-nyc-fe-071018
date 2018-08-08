f9c94f4a4a2ca3986a09000fd71986c7f4434a61
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-08 22:07:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser = "David";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.string(0,1)}!`;
+const shortGreeting = `Welcome, ${currentUser.string[0,1]}!`;
